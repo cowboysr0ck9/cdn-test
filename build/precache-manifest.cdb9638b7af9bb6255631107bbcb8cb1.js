df10c2c9b4dd4cb758b35897b8d8470285a3dabb
Tyler Eads <tylereads@Tylers-MacBook-Pro.local>
2020-03-21 21:47:56 -0400

    Added Build for CDN

---
+++ build/precache-manifest.cdb9638b7af9bb6255631107bbcb8cb1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "480a25d705b8b499cebf069f038065f1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c094170b47ba25a4389c",
+    "url": "/static/css/main.7bd4f7e1.chunk.css"
+  },
+  {
+    "revision": "812ac87b5e18ec50e57b",
+    "url": "/static/js/2.48cf00e7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.48cf00e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c094170b47ba25a4389c",
+    "url": "/static/js/main.61bb8249.chunk.js"
+  },
+  {
+    "revision": "44ceae032aaa4e9eb76f",
+    "url": "/static/js/runtime-main.094808c2.js"
+  }
+]);
--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import { App } from "./App";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// IIFE to create application DOM target
+const el = "cdn-app";
+(() => {
+  const cdn = document.createElement("DIV");
+  cdn.setAttribute("id", el);
+  document.body.append(cdn);
+})();
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(<App />, document.querySelector(`#${el}`));
